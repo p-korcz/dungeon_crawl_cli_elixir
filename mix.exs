55e0f785a4717a0e147e95299fcaf08f203190c2
Przemek <arundel@op.pl>
2019-12-20 16:12:38 +0100

    dialyxir removed

--- mix.exs
+++ mix.exs
@@ -9,7 +9,6 @@ defmodule DungeonCrawl.MixProject do
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
       deps: deps(),
-      dialyzer: [plt_add_apps: [:mix]]
     ]
   end
 
@@ -23,7 +22,6 @@ defmodule DungeonCrawl.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
